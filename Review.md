d9e1ac17d60e2b058cf79a0854e2e7b8c671c426
bit716 <sch030518@gmail.com>
2020-08-12 06:15:39 +0900

    JFrame size, position

--- Review.md
+++ Review.md
@@ -16,3 +16,18 @@ frame.setVisible(true);
 
 **기존에 '이전에 실행시켰던 어플리케이션 시작'으로 되어있음**
 
+## 2. 자바 GUI JFrame 크기, 위치 및 좌표계
+### JFrame 위치 지정
+```
+JFrame.setLocation(int x, int y)
+```
+### Java 의 좌표계
+* 우측으로 갈수록 X 값이 증가
+* 하단으로 갈수록 Y 값이 증가
+
+### JFrame 크기 조절
+```
+Dimension(int x, int y);
+JFrame.setPrefferedSize(Dimension);
+```
+**크기 조절 이후에 JFrame.pack() 을 해주지 않으면 적용 되지 않음**
--- SwingPractice/src/me/Jun/SwingPractice/main/Example.java
+++ SwingPractice/src/me/Jun/SwingPractice/main/Example.java
@@ -1,5 +1,7 @@
 package me.Jun.SwingPractice.main;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -7,10 +9,14 @@ public class Example
 {
 	public static void main(String args[])
 	{
+		Dimension dim = new Dimension(500, 300);
+		
 		JFrame frame = new JFrame("Jun's House");
 		JLabel label = new JLabel("Hello world");
 		frame.add(label);
+		frame.setPreferredSize(dim);
 		frame.pack();
+		frame.setLocation(200, 400);
 		frame.setVisible(true);
 	}
 }
